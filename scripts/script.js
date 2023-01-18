bd70181a4b3cdd34c777bcf3a56c9231e3c82938
tardmonkey <guillain.benoist@gmail.com>
2023-01-18 13:20:46 +0100

    arrow left

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,26 @@
 //boutons 
 
-let quiSommesNous = document.getElementById("bouton__qui")
 
-quiSommesNous.addEventListener("click", pageQui)
 
-console.log(document.getElementById("about"))
+document.getElementById("bouton__qui").addEventListener("click", pageQui)
+document.getElementById("bouton__contact").addEventListener("click", pageContact)
+document.getElementById("arrow-left").addEventListener("click", pageAccueil)
+
 
 function pageQui() {
     document.querySelector("main").setAttribute("class", "hide")
     document.getElementById("about").style.display = "flex"
     document.getElementById("about").setAttribute("class", "about--clicked")
+}
+
+function pageContact(){
+    document.querySelector("main").setAttribute("class", "hide")
+    document.getElementById("contact").style.display = "flex"
+    document.getElementById("contact").setAttribute("class", "about--clicked")
+}
+
+function pageAccueil(){
+    document.querySelector("main").removeAttribute("class", "hide")
+    document.getElementById("about").style.display = "none"
+    document.getElementById("about").removeAttribute("class", "about--clicked")
 }
