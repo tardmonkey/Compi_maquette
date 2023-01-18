cf16866bdbf912a2f0ffd6559c12de51ad224ec1
tardmonkey <guillain.benoist@gmail.com>
2023-01-18 13:45:48 +0100

    added contact

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,32 @@
-//boutons 
+window.addEventListener('load', function (){
+
+    //boutons 
+
+    document.getElementById("bouton__qui").addEventListener("click", pageQui)
+    document.getElementById("bouton__contact").addEventListener("click", pageContact)
+    document.getElementById("arrow-left").addEventListener("click", pageAccueil)
 
 
+    function pageQui() {
+        document.querySelector("main").setAttribute("class", "hide")
+        document.getElementById("about").style.display = "flex"
+        document.getElementById("about").setAttribute("class", "about--clicked")
+    }
 
-document.getElementById("bouton__qui").addEventListener("click", pageQui)
-document.getElementById("bouton__contact").addEventListener("click", pageContact)
-document.getElementById("arrow-left").addEventListener("click", pageAccueil)
+    function pageContact(){
+        document.querySelector("main").setAttribute("class", "hide")
+        document.getElementById("contact").style.display = "flex"
+        document.getElementById("contact").setAttribute("class", "about--clicked")
+    }
 
+    function pageAccueil(){
+        document.querySelector("main").removeAttribute("class", "hide")
+        document.getElementById("about").style.display = "none"
+        document.getElementById("about").removeAttribute("class", "about--clicked")
+        document.getElementById("contact").style.display = "none"
+        document.getElementById("contact").removeAttribute("class", "about--clicked")
 
-function pageQui() {
-    document.querySelector("main").setAttribute("class", "hide")
-    document.getElementById("about").style.display = "flex"
-    document.getElementById("about").setAttribute("class", "about--clicked")
-}
+        console.Log("arrow clicked")
+    }
 
-function pageContact(){
-    document.querySelector("main").setAttribute("class", "hide")
-    document.getElementById("contact").style.display = "flex"
-    document.getElementById("contact").setAttribute("class", "about--clicked")
-}
-
-function pageAccueil(){
-    document.querySelector("main").removeAttribute("class", "hide")
-    document.getElementById("about").style.display = "none"
-    document.getElementById("about").removeAttribute("class", "about--clicked")
-}
+})
