aaabf3dd709b1876683cde0af36ea05b9b944bc2
tardmonkey <guillain.benoist@gmail.com>
2023-01-30 16:51:24 +0100

    commence le mailing

---
+++ back/app.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+const sgMail = require('@sendgrid/mail')
+const http = require('http');
+
+// sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+// const mailConfirm = {
+//     from: FROM_EMAIL_ADDRESS,
+//     template_id: "d-52417acfcf0d42f8a5eb603f46b7b2af",
+//     to: FROM_EMAIL_ADDRESS
+
+//   };
+//     sgMail
+//         .send(mailConfirm)
+//         .then(() => {
+//             console.log('Email sent')
+//         })
+//         .catch((error) => {
+//             console.error(error)
+//         })
+
+
+
+
+   
+
